d1349c88598fed5853f839269c25421fd273c468
Chris Olstrom <chris@olstrom.com>
2016-08-13 22:13:45 -0700

    Add HyperDock::Resource::Projects

---
+++ lib/hyperdock/resource/projects.rb
@@ -0,0 +1,26 @@
+require 'docker-api'
+require_relative 'core'
+
+module HyperDock
+  module Resource
+    class Projects < Core
+      Contract None => ArrayOf[String]
+      def projects
+        @projects ||= ::Docker::Container.all.map do |container|
+          container.info.dig 'Labels', 'com.docker.compose.project'
+        end.uniq
+      end
+
+      Contract None => HashOf[RespondTo[:to_s], Hash]
+      def links
+        @links ||= projects.map do |project|
+          { "project:#{project}" => { href: "/project/#{project}" } }
+        end.reduce(&:merge)
+      end
+
+      def attributes
+        { projects: projects }
+      end
+    end
+  end
+end
