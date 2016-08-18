5a14693e1b3c21e9dc85a5bf53515da51572a356
Chris Olstrom <chris@olstrom.com>
2016-08-17 17:53:25 -0700

    Removed ProjectServices, merged into Project

--- lib/hyperdock/resource/project.rb
+++ lib/hyperdock/resource/project.rb
@@ -4,22 +4,52 @@ require_relative 'core'
 module HyperDock
   module Resource
     class Project < Core
-      Contract None => ArrayOf[String]
-      def projects
-        @projects ||= ::Docker::Container.all.map do |container|
-          container.info.dig 'Labels', 'com.docker.compose.project'
+      Contract None => String
+      def project
+        @project ||= request.path_info[:project]
+      end
+
+      Contract None => ArrayOf[::Docker::Container]
+      def containers
+        @containers ||= ::Docker::Container.all.select do |container|
+          container.info.dig('Labels', 'com.docker.compose.project') == project
         end
       end
 
       Contract None => Bool
       def resource_exists?
-        projects.include? request.path_info[:project]
+        !containers.empty?
+      end
+
+      Contract None => ArrayOf[String]
+      def services
+        @services ||= containers.map do |container|
+          container.info.dig('Labels', 'com.docker.compose.service')
+        end.uniq
+      end
+
+      Contract None => ArrayOf[Hash]
+      def service_links
+        @service_links ||= services.map do |service|
+          { name: service, href: "/project/#{project}/service/#{service}" }
+        end
+      end
+
+      Contract None => Hash
+      def named_links
+        @named_links ||= service_links.map do |service|
+          { "service:#{service[:name]}" => service }
+        end.reduce(&:merge)
       end
 
       def links
         @links ||= {
-          services: { href: "/#{request.disp_path}/services" }
-        }
+          services: service_links
+        }.merge(named_links)
+      end
+
+      def attributes
+        { names: services }
       end
     end
   end
